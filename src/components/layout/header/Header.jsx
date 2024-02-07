5ffc3e5e3f6f560fdaf5f241dff23030345b91cc
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-02-07 00:12:19 -0300

    Logout button and user data added in header

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./header.css";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { Button } from "@mui/material";
 
 export const Header = () => {
-  return <div className="header_container">Header</div>;
+  const { state, logout } = useContext(AuthContext);
+  const { isLogged, user } = state;
+  return (
+    <div className="header_container">
+      {isLogged && (
+        <>
+          hola {user.name} your role is : {user.role} and your id is: {user.id}
+          <Button variant="contained" onClick={logout}>
+            Logout
+          </Button>
+        </>
+      )}
+      {!isLogged && <div>HEADER</div>}
+    </div>
+  );
 };
