22935f5ee257ba3f0d454f3a84412de7330971f3
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-03-11 11:51:35 +1030

    authSlice with thunks added header and login modified

---
+++ src/store/slices/auth/authSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    user: {},
+    isLogged: false,
+    token: "",
+    message: "",
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload.user,
+              state.isLogged= true,
+              state.token= action.payload.token,
+              state.message= "User Logged successfully"
+        },
+        logout: (state) => {
+            state.user = null
+            state.isLogged= false
+            /* {
+                name: null,
+                email: null,
+                password: null,
+                createAt: null,
+                isDelete: null,
+                role: null
+            } */
+        },
+    },
+})
+
+export const { login, logout } = authSlice.actions;
---
+++ src/store/slices/auth/thunks.js
@@ -0,0 +1,52 @@
+import { clientAxios } from "../../../api/ClientAxios";
+import { login, logout } from "./authSlice";
+
+
+export const getLogin = (email, password) => {
+    return async(dispatch) => {
+        try {
+            const { data: dataToken } = await clientAxios.post("/user/login", {
+              email,
+              password,
+            });
+            localStorage.setItem("jwtoken", dataToken.token);
+            const { data: dataUser } = await clientAxios.get("/user/userData");
+        dispatch(login({
+            user: {
+                id: dataUser.userFound._id,
+                name: dataUser.userFound.name,
+                role: dataUser.userFound.role,
+              },
+            token: dataToken.token            
+            
+        }
+        ));
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export const checkTocken = () => {
+    return async (dispatch) => {
+        const token = localStorage.getItem("jwtoken");
+        if (!token) {
+          dispatch(logout());
+        } else {
+          try {
+            const { data: dataUser } = await clientAxios.get("/user/userData");
+            if (dataUser) {
+              dispatch(login({
+                user: {
+                    id: dataUser.userFound._id,
+                    name: dataUser.userFound.name,
+                    role: dataUser.userFound.role,
+                  },
+                token: token            
+            }
+            ));
+            } 
+}   catch (error) {
+    console.log(error)
+}
+}}}
