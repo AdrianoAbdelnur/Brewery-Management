74e5ec5465f6220d03ef6aa8af44ca5815710192
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-03-16 18:03:10 +1030

    Details barrel modal added

---
+++ src/components/modals/DetailsBarrelsModal.jsx
@@ -0,0 +1,63 @@
+import { Box, Grid, Modal, Typography } from "@mui/material";
+import React from "react";
+import { Table } from "../table/Table";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "60%",
+  bgcolor: "#0c0c0c",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  color: "white",
+};
+
+export const DetailsBarrelsModal = ({ show, handleClose, rows }) => {
+  console.log(rows);
+  const columns = [
+    {
+      field: "order",
+      headerName: "#",
+      width: 60,
+      hideable: false,
+    },
+    {
+      field: "cap",
+      type: "number",
+      headerName: "Capacity",
+      width: 100,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 200,
+    },
+    {
+      field: "deliDate",
+      headerName: "Delivered Date",
+      width: 150,
+    },
+    {
+      field: "style",
+      headerName: "style",
+      width: 150,
+    },
+  ];
+
+  return (
+    <Modal
+      open={show}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <h2>BAR: {rows[0]?.customer}</h2>
+        <Table columns={columns} rows={rows} toolbar={false} />
+      </Box>
+    </Modal>
+  );
+};
