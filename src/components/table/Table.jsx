4546c7c7d5593e75941a87723ed46707f68b4280
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-03-16 18:05:35 +1030

    table modifications

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,30 +1,53 @@
-import { Box, Icon } from "@mui/material";
+import { Box } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import React from "react";
 
-export const Table = ({ columns, rows }) => {
+export const Table = ({ columns, rows, cellsStyle = null, toolbar = true }) => {
   return (
     <Box
       sx={{
         maxHeight: 700,
-        width: "90vw",
+        width: "92%",
         background: "#212121",
         borderRadius: ".5rem",
         margin: "2rem",
+        "& .inTime": {
+          backgroundColor: "#b9d5ff91",
+          color: "#ffffff",
+        },
+        "& .exceded": {
+          backgroundColor: "#950101",
+          color: "#ffffff",
+        },
       }}
     >
       <DataGrid
         rows={rows}
+        rowHeight={40}
         columns={columns}
         getRowId={(row) => row?._id}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
-        pageSizeOptions={[5, 10]}
-        slots={{
-          toolbar: GridToolbar,
+        pageSizeOptions={[10, 20]}
+        slots={
+          toolbar
+            ? {
+                toolbar: GridToolbar,
+              }
+            : ""
+        }
+        getCellClassName={(params) => {
+          if (
+            params.field === "barName" ||
+            params.value == null ||
+            cellsStyle !== "barrels"
+          ) {
+            return "";
+          }
+          return params.value >= 1 ? "exceded" : "inTime";
         }}
       />
     </Box>
