89e4265a992602574c00df666e9d237f0accf74f
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2024-03-23 13:38:18 +1030

    stylesSlice and thunks added

---
+++ src/store/slices/beerStyles/thunks.js
@@ -0,0 +1,28 @@
+import { clientAxios } from "../../../api/ClientAxios"
+import { addStyle, getStyles, stylesmsg } from "./stylesSlice"
+
+
+
+export const getAllStyles = () => {
+    return async(dispatch) => {
+        try {
+            const {data} = await clientAxios("/styles/getStyles")
+            dispatch(getStyles({styles : data.stylesFound}))
+        } catch (error) {
+            console.log(error)
+        }
+}}
+
+export const addNewStyle = (styleInfo) => {
+    return async(dispatch) => {
+        try {
+            const { data } = await clientAxios.post("/styles/addNewStyle", styleInfo)
+            dispatch(addStyle(data.newStyle))
+            dispatch(stylesmsg({
+                message: data.message,
+                type: "success"
+            }))
+        } catch (error) {
+            console.log(error)
+        }
+}}
